048c9f2a642546435f1db5d3e8578397fd7a8a0f
Chaitra <chaitra.srinivas@gmail.com>
2021-09-17 21:59:50 +1000

    Saves the event to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,54 +2,31 @@
 
 var $timeDisplayEl = $("#time-display");
 var $eventInputEl = $("#event-input");
-var $saveBtnEl = $("#save-button");
-var $hourDisplayEl = $('#hour-display');
+var $saveBtnEl = $(".saveBtn");
 
 var newEvent = localStorage.getItem("event-name");
 $eventInputEl.text(newEvent);
 
-function displayTime() {
-    var rightNow = moment().format('dddd MMM DD, YYYY [at] hh:mm:ss a');
-    $timeDisplayEl.text(rightNow);
+// Displays the current date and time
+$timeDisplayEl.text(moment().format("dddd MMM DD, YYYY [at] hh:mm:ss a"));
+
+$saveBtnEl.on("click", function () {
+  var eventTime = $(this).parent().attr("id");
+  var textValue = $(this).siblings("#event-input").val();
+  localStorage.setItem(eventTime, textValue);
+  console.log(eventTime + textValue + "Setting values");
+});
+
+function refreshPage() {
+    $eventInputEl.each(function () {
+    var eventTime = $(this).parent().attr("id");
+    var textValue = localStorage.getItem(eventTime);
+
+    if(textValue !== null){
+       $(this).val(textValue);
+       /*  $(this).siblings(".description").val(textValue); */
+    }
+    console.log(eventTime + textValue + "Getting values");
+  });
 }
-
-setInterval(displayTime, 1000);
-
-/* $saveBtnEl.click( function(){
-    alert($eventInputEl.val());
-    localStorage.setItem("event-name",$eventInputEl.val());
-     return;
-    });
-
-
-
-    alert(localStorage.getItem("event-name"));
-    var savedEvent = localStorage.getItem("event-name");
-    $eventInputEl.text(savedEvent);
-
-console.log($eventInputEl.val()); */
-/* $('#myTextbox').on('input', function() {
-    // do something
-}); */
-
-
-
-$saveBtnEl.on('click', function(){
-
-    $('input[type="text"]').each(function(){    
-        var id = $(this).attr('id');
-        var value = $(this).val();
-       localStorage.setItem(id, value);
-
-    });   
-});
-
-$('#load').on('click', function(){
-    $('input[type="text"]').each(function(){    
-        var id = $(this).attr('id');
-        var value = localStorage.getItem(id);
-
-        $(this).val(value);
-
-    }); 
-});
+refreshPage();
