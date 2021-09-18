8d5290c8c0d13620254e726a3b688869fc4289ba
Chaitra <chaitra.srinivas@gmail.com>
2021-09-18 15:23:40 +1000

    timeblock colour coding based on the time of day-js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,22 @@ var $timeDisplayEl = $("#time-display");
 var $eventInputEl = $("#event-input");
 var $saveBtnEl = $(".saveBtn");
 var $hourEl = $(".hour");
+var $timeBlockEl = $(".time-block");
 
 // Populates data stored in local storage on page reload
 getSavedEvents();
+checkIfCurHour(); // Checks if the time block is in the past/present/future
 
 // Displays the current date and time
 $timeDisplayEl.text(moment().format("dddd MMM DD, YYYY [at] hh:mm:ss a"));
 
-// Each time save button is clicked the value in the text area is and the corresponding id is saved to the local storage
+// Each time save button is clicked the value in the text area and the corresponding id is saved to the local storage
 $saveBtnEl.on("click", function () {
     var eventTime = $(this).parent().attr("id");
     var textValue = $(this).siblings("#event-input").val();
     localStorage.setItem(eventTime, textValue);
     console.log(eventTime + textValue + "Setting values");
 });
-
 
 function getSavedEvents() {
     $hourEl.each(function () {
@@ -30,3 +31,24 @@ function getSavedEvents() {
     });
 }
 
+// Check if hour is current, past or present
+function checkIfCurHour() {
+    var currHour = "hour" + moment().hours();
+    $timeBlockEl.each(function () {
+        var blockHour = $(this).attr("id")
+
+        if (blockHour > currHour) {
+            $(this).removeClass("present");
+            $(this).addClass("past");
+            $(this).addClass("future");
+        } else if (blockHour < currHour) {
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        } else if (blockHour === currHour) {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+    });
+}
